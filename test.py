cc877553c826fef38440cb7d5d09e00a14b64793
Ibrahim.toubeh <ibrahim.toubeh@optimumpartners.co>
2025-12-10 19:59:32 +0300

    task completed

--- test.py
+++ test.py
@@ -1,13 +1,16 @@
 from trainingday1 import BankAccount
+
 acc1 = BankAccount("Alice", 1000, "1234")
 acc1.deposit(500)
 acc1.withdraw(200, "1234")
+
 print("Balance:", acc1.show_balance("1234"))
 print("Transactions:", acc1.transaction_history)
 
 acc2 = BankAccount.from_string("Bob,1500,5678")
 acc2.deposit(300)
 acc2.withdraw(100, "5678")
+
 print("Balance:", acc2.show_balance("5678"))
 print("Transactions:", acc2.transaction_history)
 
